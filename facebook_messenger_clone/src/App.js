a18e8a17c5f8895cec91b7d81787f3152085cacd
michaelrizzo2 <m_rizzo@live.com>
2020-08-03 19:11:04 -0400

    Fully setup input.

--- facebook_messenger_clone/src/App.js
+++ facebook_messenger_clone/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+  const [input,Setinput]=useState("")
   return (
     <div className="App">
     <h1>Hello Programmers</h1>
     {/*input field */}
     {/* button*/}
     {/* the messages*/}
-    <input /> 
+    <input  value={input} onChange={event=>Setinput()}/> 
     <button> Send Message</button>
     </div>
   );
