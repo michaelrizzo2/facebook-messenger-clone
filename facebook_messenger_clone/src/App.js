ff26a489cf436f7e1ba1cb96f51228e424cb8e85
michaelrizzo2 <m_rizzo@live.com>
2020-08-03 21:55:51 -0400

    Will continue toimorrow morning.

--- facebook_messenger_clone/src/App.js
+++ facebook_messenger_clone/src/App.js
@@ -6,15 +6,17 @@ function App() {
   const [messages,setmessages]=useState([])
   const sendMessage=event =>
   {
+    event.preventDefault()
     setmessages([...messages,input])
     Setinput('')
   }
   return (
     <div className="App">
     <h1>Hello Programmers</h1>
-
+    <form>
     <input  value={input} onChange={event=>Setinput(event.target.value)}/> 
-    <button onClick={sendMessage}> Send Message</button>
+    <button type="submit" onClick={sendMessage}> Send Message</button>
+    </form>
     {/* the messages*/}
     {messages.map(message =>(
       <p>{message}</p>
