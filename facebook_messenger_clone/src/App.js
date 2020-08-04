d9536a80e577385f251c5c9035272e6829f69773
michaelrizzo2 <m_rizzo@live.com>
2020-08-04 10:36:48 -0400

    Imported utilities for form control.

--- facebook_messenger_clone/src/App.js
+++ facebook_messenger_clone/src/App.js
@@ -1,6 +1,7 @@
 import React,{useState} from 'react';
 import './App.css';
 import {Button} from "@material-ui/core"
+import {FormControl,Input,InputLabel,FormHelperText} from "@material-ui/core"
 
 function App() {
   const [input,Setinput]=useState("")
@@ -15,6 +16,10 @@ function App() {
     <div className="App">
     <h1>Hello Programmers</h1>
     <form>
+      <FormControl> <InputLabel >Email address</InputLabel>
+      <Input />
+      <FormHelperText >We'll never share your email.</FormHelperText>
+      </FormControl>
     <input  value={input} onChange={event=>Setinput(event.target.value)}/> 
     <Button disabled={!input} type="submit" variant="outlined" color="primary" onClick={sendMessage}> Send Message</Button>
     </form>
