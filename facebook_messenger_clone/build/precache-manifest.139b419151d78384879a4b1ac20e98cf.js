73580a0b2575c3efdeb6090385ab8afc367df5d1
michaelrizzo2 <m_rizzo@live.com>
2020-08-06 17:19:35 -0400

    Build files

---
+++ facebook_messenger_clone/build/precache-manifest.139b419151d78384879a4b1ac20e98cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2751739e506138e03e8f6b873279aee4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a92472cab2a9a7e7c8b1",
+    "url": "/static/css/main.90473f4b.chunk.css"
+  },
+  {
+    "revision": "3d69d12847056d7c8bf4",
+    "url": "/static/js/2.594b6429.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.594b6429.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a92472cab2a9a7e7c8b1",
+    "url": "/static/js/main.8a9a8f26.chunk.js"
+  },
+  {
+    "revision": "6d3a44a64b97d13e3d47",
+    "url": "/static/js/runtime-main.28dd6377.js"
+  }
+]);
