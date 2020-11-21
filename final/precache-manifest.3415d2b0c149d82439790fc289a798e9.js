faa32fbcfa7fb3fd5426309e3f3696e730a31629
michaelrizzo2 <m_rizzo@live.com>
2020-11-21 13:48:17 -0500

    Final deploy

---
+++ final/precache-manifest.3415d2b0c149d82439790fc289a798e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a897acfc06b0678bd6da4b3f0fd1a27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d1acfdc55546438d144",
+    "url": "/static/css/main.90473f4b.chunk.css"
+  },
+  {
+    "revision": "b7271d0f8109e468b777",
+    "url": "/static/js/2.523745e0.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/static/js/2.523745e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d1acfdc55546438d144",
+    "url": "/static/js/main.68e39f5f.chunk.js"
+  },
+  {
+    "revision": "6d3a44a64b97d13e3d47",
+    "url": "/static/js/runtime-main.28dd6377.js"
+  }
+]);
